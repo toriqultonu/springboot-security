8a934920e3969e8396642eb9ec66f740bfb83d59
toriqultonu <toriqultonu22@gmail.com>
2024-02-28 20:54:27 +0600

    first table created

---
+++ src/main/java/com/example/springboot_security/user/User.java
@@ -0,0 +1,24 @@
+package com.example.springboot_security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
