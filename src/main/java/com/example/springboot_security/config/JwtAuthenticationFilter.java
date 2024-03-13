9d90c7d703a586d037bce6afa43ae5b5a7f92d3c
toriqultonu <toriqultonu22@gmail.com>
2024-03-14 03:40:37 +0600

    jwt claim extractor done

--- src/main/java/com/example/springboot_security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springboot_security/config/JwtAuthenticationFilter.java
@@ -5,11 +5,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
--- src/main/java/com/example/springboot_security/config/JwtService.java
+++ src/main/java/com/example/springboot_security/config/JwtService.java
@@ -2,6 +2,7 @@ package com.example.springboot_security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.impl.lang.Function;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
@@ -11,15 +12,22 @@ import java.security.Key;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "AE317122627DCB24B8E7FF86E595A";
+    private static final String SECRET_KEY = "7d8ff7bf10bcbf60747f7db963452a91e3d18523ae1d2c24096a21b66f0e17af";
 
     public String extractUsername(String token) {
-        return null;
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token){
         return Jwts.parser().setSigningKey(getSignInKey()).build().parseClaimsJwt(token).getBody();
     }
+
 
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
